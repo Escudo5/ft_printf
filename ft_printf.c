266bc2cdc111c5ddb829b592b4db50880d2e879d
Escudo5 <sergiomargar03@gmail.com>
2024-10-21 16:42:19 +0200

    ultimo commit de hoy

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:41:39 by smarquez          #+#    #+#             */
-/*   Updated: 2024/10/21 15:49:44 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/10/21 16:23:04 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,14 @@ static int	specifier_ft(char specifier, va_list args)
 	else if (specifier == 'u')
 		return (ft_putnbru(va_arg(args, int)));
 	else if (specifier == 'p')
-		return (ft_putptr(va_arg(args, int)));
+		return (ft_putptr(va_arg(args, void *)));
 	else if (specifier == 'x')
 		return (ft_hexadec(va_arg(args, int)));
 	else if (specifier == 'X')
 		return (ft_hexadec_u(va_arg(args, int)));
 	else if (specifier == '%')
 		return (ft_putchar('%'));
+	return (0);
 }
 char	data_type(const char *format)
 {
@@ -41,6 +42,7 @@ int	ft_printf(char const *format, ...)
 	va_list	vargs;
 	char	specifier;
 	int		result;
+	result = 0;
 
 	va_start(vargs, format);
 	while (*format)
@@ -51,5 +53,14 @@ int	ft_printf(char const *format, ...)
 			format++;
 			result += specifier_ft(specifier, vargs);
 		}
+		else 
+		{
+			write(1, format, 1);
+			result++;
+		}
+		format++;
 	}
+	va_end(vargs);
+	return (result);
 }
+
